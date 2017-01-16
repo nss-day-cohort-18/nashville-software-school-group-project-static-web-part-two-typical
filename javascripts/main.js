fdcc55d7d672e2374140343f6cc958ed3e7df935
Aaron Barfoot <aaronbarfoot@Aarons-MacBook-Pro.local>
2017-01-16 13:59:32 -0600

    start work on main.js

---
+++ javascripts/main.js
@@ -0,0 +1,74 @@
+var blocks = [
+	{
+	name: "Block 0",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: ","
+	},
+	{
+	name: "Block 1",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: ","
+	},
+	{
+	name: "Block 2",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: " ",
+	},
+	{
+	name: "Block 3",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 4",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 5",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 6",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 7",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 8",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 9",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 10",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+	{
+	name: "Block 11",
+	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+	price: "$99",
+	url: "img/",
+	},
+];
