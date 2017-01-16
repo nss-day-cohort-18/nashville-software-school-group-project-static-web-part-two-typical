5fef6a94c0a67d04c35d63581bf6edd5fe7b9910
“Aaron <barfootaaron@yahoo.com>
2017-01-16 14:49:41 -0600

    changes to main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,22 @@
+/* Initialize and manually populate array */
 var blocks = [
 	{
 	name: "Block 0",
 	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 	price: "$99",
-	url: ","
+	url: "img/",
 	},
 	{
 	name: "Block 1",
 	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 	price: "$99",
-	url: ","
+	url: "img/",
 	},
 	{
 	name: "Block 2",
 	description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 	price: "$99",
-	url: " ",
+	url: "img/",
 	},
 	{
 	name: "Block 3",
@@ -72,3 +73,24 @@ var blocks = [
 	url: "img/",
 	},
 ];
+
+/* Initialize a loop, which continues for the length of the array */
+for (var i=0; i<blocks.length; i++) {
+
+	/*Create Separate DOM Elements for the Data */
+	var card = document.getElementById("productCard" + i);
+
+	/* Create a DOM Element for the Name */
+	var blockName = "<h3>" + blocks[i].name + "</h3";
+
+	/* Populate the Element with Data */
+	card.innerHTML += blockName;
+
+	/* Wash, Rinse, Repeat. . .*/
+	var blockImage = '<img src="' + blocks[i].URL + '">';
+	card.innerHTML += blockImage;
+	var blockDescription = "<p>" + blocks[i].description + "</p>";
+	card.innerHTML += blockDescription;
+	var blockPrice = "<p>" + blocks[i].price + "</p>";
+	card.innerHTML += blockPrice;
+}
