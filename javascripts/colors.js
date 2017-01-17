48c4b3768e0c1abb0fc1b0b217a6c0a38acdb025
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-01-17 11:00:38 -0600

    Some slight debugging work still to be done.

--- javascripts/colors.js
+++ javascripts/colors.js
@@ -10,7 +10,7 @@ var productsBlueColors = ["#567CF9", "#5454ED", "#9793FF", "#86E3DE", "#60EFFF",
 // Contact page color
 var contactsGreenColors = ["#F1F2FD", "#0B9C17", "#2EEF7E", "#AFFCA0", "#9EBE8C", "#B8C2B7", "#CBF9CF", "#00F420", "#126E0C", "#FFFFFF"];
 // home page colors
-var rainbowColors = ["#BF3F3F", "#F6BABA", "#FFFFFF", "#567CF9", "#5454ED", "#9793FF", "#F1F2FD", "#0B9C17", "#2EEF7E", "#ECF62C", "#FFDB28", "#FB28FF", "#923DCB", "#AB6AD6", "#FFFB19"];
+// var rainbowColors = ["#BF3F3F", "#F6BABA", "#FFFFFF", "#567CF9", "#5454ED", "#9793FF", "#F1F2FD", "#0B9C17", "#2EEF7E", "#ECF62C", "#FFDB28", "#FB28FF", "#923DCB", "#AB6AD6", "#FFFB19"];
 
 
 //combine color pallettes for home
@@ -37,10 +37,6 @@ console.log(combinedColors.length);
 function getRandomInt(min, max) {
   return (Math.floor(Math.random() * (max - min + 1)) + min);
 }
-var randomCombinedColor = getRandomInt(0, combinedColors.length - 1);
-var randomAboutColor = getRandomInt(0, aboutRedColors.length - 1);
-var randomProductsColor = getRandomInt(0, productsBlueColors.length - 1);
-var randomContactsColor = getRandomInt(0, contactsGreenColors.length - 1);
 
 
 //Creating two new arrays to set pre-determined 
@@ -60,56 +56,69 @@ var mainContentHeightRemainder = (heightMinusFooterAndMargin % 10);
 var sidebarHeight = (heightMinusFooterAndMargin - mainContentHeightRemainder);
 console.log("Main content height: ", mainContentHeight, "Main content remainder: ", mainContentHeightRemainder, "Sidebar height: ", sidebarHeight);
 
-//grab parent element and setting height
-var indexSidebar = document.getElementById("sidebar1");
-indexSidebar.style.height = sidebarHeight + "px";
+var createSidebarBlocks = function(sidebarName) {
 
-		
-//Beginning of random div function
-//Create counter to keep track of stacking
-//div height
-var counter = 0;
+	//Create counter to keep track of stacking
+    //div height
+    var counter = 0;
 
+    //Beginning of random div function
+	while (counter < sidebarHeight) {
 
-function createSidebarBlocks() {
-	//check to see if there is room to add divs
-	   do {
-		//Detrmining how much room I have to work with
-		var sidebarHeightRemainder = sidebarHeight - counter;
+    	//grab parent element and setting height
+        var newSidebar = document.getElementById(sidebarName);
+        newSidebar.style.height = sidebarHeight + "px";
+    
+    	//check to see if there is room to add divs
+    	//Detrmining how much room I have to work with
+    	var sidebarHeightRemainder = sidebarHeight - counter;
 
-		//grabbing random number based on height array.length
-		var divHeightIndex = getRandomInt(0, sidebarBlockHeight.length - 1);
-		var divHeight = sidebarBlockHeight[divHeightIndex];
+    	//grabbing random number based on height array.length
+    	var divHeightIndex = getRandomInt(0, sidebarBlockHeight.length - 1);
+    	var divHeight = sidebarBlockHeight[divHeightIndex];
 
-		//If there is space for this random div.. 
-		if ((sidebarHeightRemainder - divHeight) > 0) {
-			counter += divHeight;
+    	//If there is space for this random div.. 
+    	if ((sidebarHeightRemainder - divHeight) > 0) {
 
-			//get random width from width array
-			var divWidthIndex = getRandomInt(0, sidebarBlockWidth.length - 1);
-			var divWidth = sidebarBlockWidth[divWidthIndex];
-			 //get random color
-			var divColor = randomCombinedColor;
+    		counter += divHeight;
+            console.log(counter);
 
-			//create div to go in sidebar
-			var sidebarDiv = document.createElement("div");
-			//assign class
-			sidebarDiv.className = "sidebarBlock";
-			//assign height and width styling
-			sidebarDiv.style.height = divHeight + "px";
-			sidebarDiv.style.width = divWidth + "px";
-			//assign background color
-			sidebarDiv.style.backgroundColor = divColor;
+    		//get random width from width array
+    		var divWidthIndex = getRandomInt(0, sidebarBlockWidth.length - 1);
+    		var divWidth = sidebarBlockWidth[divWidthIndex];
+    		 //get random color
+    		var randomCombinedColor = getRandomInt(0, combinedColors.length - 1);
+            var randomAboutColor = getRandomInt(0, aboutRedColors.length - 1);
+            var randomProductsColor = getRandomInt(0, productsBlueColors.length - 1);
+            var randomContactsColor = getRandomInt(0, contactsGreenColors.length - 1);
 
-			//append block to sidebar
-			indexSidebar.appendChild(sidebarDiv);
+    		//create div to go in sidebar
+    		var sidebarDiv = document.createElement("div");
+    		//assign class
+            if (sidebarName === "sidebar1") {
+        		sidebarDiv.className = "sidebarBlockLeft";
+            } else if (sidebarName === "sidebar2") {
+                sidebarDiv.className = "sidebarBlockRight";
+            };
 
-		} while (counter < sidebarHeight);
-	}
+    		//assign height and width styling
+    		sidebarDiv.style.height = divHeight + "px";
+    		sidebarDiv.style.width = divWidth + "px";
+    		//assign background color
+    		sidebarDiv.style.backgroundColor = combinedColors[randomCombinedColor];
+
+    		//append block to sidebar
+    		newSidebar.appendChild(sidebarDiv);
+        } else {
+            newFunction(sidebarName);
+	    }
+	} 
 }
 
 //call function
-createSidebarBlocks();
+createSidebarBlocks("sidebar1");
+console.log("function 1 is good")
+createSidebarBlocks("sidebar2");
 
 
 
